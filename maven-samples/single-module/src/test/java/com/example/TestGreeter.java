1ac09a37d66814069e0013f969079cf87abdfac2
manojsap <160022548+manojsap@users.noreply.github.com>
2024-04-15 21:14:13 +0530

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -35,10 +35,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-  @Test
-  public void intentionallyFailed() {
-    assertTrue(true);
-  }
+
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
